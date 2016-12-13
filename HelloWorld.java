cff2f1b243a4701c369750db04f9d1c1b52fbb5a
Vinay Krishna <vinaykris@gmail.com>
2016-12-13 22:49:16 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,15 @@
 public class HelloWorld {
-    public static void main(String[] args) {
+    private String message;
+    
+    public HelloWorld(String message){
+        this.message = message;
+    }
+    /*public static void main(String[] args) {
         System.out.println("Hello World");
+    }*/
+    
+    public String printMessage(){
+        System.out.println(message);
+        return message
     }
 }
